978614d7f24a5cbe137051ba95343d5ec556fc0a
sumit <sumitburnwal22@gmail.com>
2020-11-17 16:57:35 +0530

    balanced bracket

--- random programs/star1.py
+++ random programs/star1.py
@@ -1,19 +1,39 @@
-# #Star
-# for i in range(0,6):
-#     for j in range(0,i+1):
-#         print('*',end="")
-#     print("\r")
+#Star
+print ("Star")
+for i in range(0,6):
+    for j in range(0,i+1):
+        print('*',end="")
+    print("\r")
 
 
-# # Inverted Star
-# for i in range(6,0,-1):
-#     print(i*"*")
+# Inverted Star
+print("Inverted Star")
+for i in range(6,0,-1):
+    print(i*"*")
 
 # Star Pyramid
-n=5
+print("Star Pyramid")
+n=7
 for i in range(0,n):
     for j in range(0,n-i-1):
         print(end=" ")
     for k in range(0,i+1):
         print("*",end=" ")
     print()
+
+# Inverted Star Pyramid
+print("Inverted Star Pyramid")
+n=7
+for i in range(n,0,-1):
+    for j in range(0,n-i):
+        print(end=" ")
+    for k in range(0,i):
+        print("*",end=" ")
+    print()
+
+#Right Indented Star
+print("Right Indented Star")
+n=5
+for i in range(0,n):
+    print(" "*(n-i+1),end="")
+    print("*"*(i+1))
